2446e5edd512d55e884e1b9c8f8e350dabeefac1
Kévin Rocha <neonix75@gmail.com>
2020-08-24 11:23:39 +0200

    Link the countries API to the dropdown

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 import './App.css';
 
 function App() {
+
+  const apiURL = "https://disease.sh/v3/covid-19/countries";
+
+  // Variables
+  const [countries, setCountries] = useState([]);
+  
+
+  // API call to get the countries
+  useEffect(() => {
+    async function getCountriesData() {
+      await fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+          // Return an object with the name and the code of the country
+          const countries = data.map(country => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }
+          ));
+
+          setCountries(countries);
+        })
+    }
+
+    getCountriesData();
+  }, []);
+  
+
+
   return (
     <div className="App">
       <div className="app__header">
@@ -10,10 +40,11 @@ function App() {
 
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
+            
+            {countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+
           </Select>
         </FormControl>
       </div>
