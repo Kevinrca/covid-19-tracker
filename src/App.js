93e0ec0fd2a83a57215ab19cf7121fab10fbd64b
Kévin Rocha <neonix75@gmail.com>
2020-08-24 10:53:45 +0200

    Small styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>COVID-19 Tracker</h1>
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      
     </div>
   );
 }
